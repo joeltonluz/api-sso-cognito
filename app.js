df0eb6f80ccddff20cd48579e51bfa8811d17077
joeltonluz <joelton.luz@bughunt.com.br>
2024-10-14 11:19:42 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const AWS = require('aws-sdk');
+
+const app = express();
+app.use(bodyParser.json());
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
+
+const cognito = new AWS.CognitoIdentityServiceProvider({
+  region: process.env.AWS_REGION
+});
+
+cognito.updateUserPoolClient
+
+app.post("/api/auth/create-idp", async (req, res) => {
+  const { providerName, metadataUrl } = req.body;
+  try {
+    const params = {
+      UserPoolId: process.env.AWS_USER_POOL_ID,
+      ProviderName: providerName, // Nome do seu provedor SAML
+      ProviderType: "SAML",
+      ProviderDetails: {
+        MetadataURL: metadataUrl,
+        // OU use MetadataFile se você tiver o arquivo de metadados localmente
+        // MetadataFile: 'conteúdo-base64-do-arquivo-de-metadados',
+      },
+      // AttributeMapping: {
+      //   'email': 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress',
+      //   'given_name': 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname',
+      //   'family_name': 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname',
+      //   // Adicione outros mapeamentos conforme necessário
+      // },
+      IdpIdentifiers: [
+        providerName, // Identificador único para este IdP, pode ser o mesmo que ProviderName
+      ],
+    };
+
+    console.log('before')
+    const result = await cognito. createIdentityProvider(params).promise();
+    console.log('IdP SAML criado com sucesso:', result);
+    return result;
+  } catch (error) {
+    console.error('Erro ao criar IdP SAML:', error);
+    res.status(500).json({ error: 'Erro ao criar IDP' });
+  }
+});
+
+app.post('/api/auth/get-idp-url', async (req, res) => {
+  const { email } = req.body;
+  const userPoolId = process.env.AWS_USER_POOL_ID;
+  const clientId = process.env.AWS_CLIENT_ID;
+
+  try {
+    const params = {
+      UserPoolId: userPoolId,
+      AttributesToGet: ['email'],
+      Filter: `email = "${email}"`
+    };
+  
+
+    cognito.createIdentityProvider()
+    const { Users } = await cognito.listUsers(params).promise();
+    console.log('🚀 ~ file: app.js:32 ~ app.post ~ Users:', JSON.stringify(Users));
+
+    if (Users && Users.length > 0) {
+      const user = Users[0];
+      if (!user.Enabled)
+        res.status(404).json({ error: 'Usuário inativo' });
+
+      console.log('🚀 ~file: app.js:69 ~ app.post ~ user:', user);
+      // Aqui você precisaria ter uma lógica para mapear o usuário ao IDP correto
+      const idpName = user.Username.split('_')[0]; // Exemplo
+      const idpUrl = `https://cogbughunt.auth.us-east-2.amazoncognito.com/oauth2/authorize?identity_provider=${idpName}&redirect_uri=https://jwt.io&response_type=token&client_id=${clientId}&scope=email+openid+profile`;
+
+      res.json({ idpUrl });
+    } else {
+      res.status(404).json({ error: 'Usuário não encontrado' });
+    }
+  } catch (error) {
+    console.error('Erro ao buscar usuário:', error.message);
+    res.status(500).json({ error: 'Erro ao processar a solicitação' });
+  }
+});
+
+app.post('/api/auth/validate-token', async (req, res) => {
+
+})
+
+app.get('/api/auth/list', async (req, res) => {
+  const result = await cognito.updateUserPoolClient({
+    SupportedIdentityProviders
+  })
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
